a211813d7545842e77299afe3b2ef8b5759f106e
David Frailey <dfrailey@empire-medical.com>
2021-12-02 11:06:10 -0800

    Implments PartTwo

--- solutions/DayTwo.py
+++ solutions/DayTwo.py
@@ -1,5 +1,15 @@
 from inputs import DayTwo
 
+class Pilot:
+    def __init__(self, submarine):
+        self.submarine = submarine
+
+    def make_it_so(self, input : list[str]):
+        for command in input:
+            method, argument = command.split(' ')
+            submarine_command = getattr(self.submarine, method)
+            submarine_command(int(argument))
+
 class Submarine:
     def __init__(self):
         self.position = [0, 0]
@@ -13,16 +23,37 @@ class Submarine:
     def up (self, amount: int):
         self.position[1] -= amount
 
+class AimableSubmarine:
+    def __init__(self):
+        self.position = {"h": 0, "d": 0, "a": 0}
+
+    def forward(self, amount : int):
+        self.position["h"] += amount
+        self.position["d"] += amount * self.position["a"]
+
+    def up(self, amount : int):
+        self.position["a"] -= amount
+
+    def down(self, amount: int):
+        self.position["a"] += amount
 class PartOne:
     def __init__(self):
         self.submarine = Submarine()
 
     def run(self, input : list[str]):
-        for command in input:
-            method, argument = command.split(' ')
-            submarine_command = getattr(self.submarine, method)
-            submarine_command(int(argument))
+        Pilot(self.submarine).make_it_so(input)
 
     def solve(self) -> int:
         self.run(DayTwo.input)
         return self.submarine.position[0] * self.submarine.position[1]
+
+class PartTwo:
+    def __init__(self):
+        self.submarine = AimableSubmarine()
+
+    def run(self, input : list[str]):
+        Pilot(self.submarine).make_it_so(input)
+
+    def solve(self) -> int:
+        self.run(DayTwo.input)
+        return self.submarine.position["h"] * self.submarine.position["d"]
--- tests/test_day_two.py
+++ tests/test_day_two.py
@@ -13,3 +13,9 @@ def test_part_one():
     sut = DayTwo.PartOne()
     sut.run(input)
     assert sut.submarine.position == [15, 10]
+
+def test_part_two():
+    sut = DayTwo.PartTwo()
+    sut.run(input)
+    assert sut.submarine.position["h"] == 15
+    assert sut.submarine.position["d"] == 60
